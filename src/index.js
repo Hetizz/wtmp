c7035e4f360f27443b11b5a08e2f750cbe2db1b8
Hetizz <heta.huostila@metropolia.fi>
2022-01-31 14:53:12 +0200

    week 3 task 1 start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('Hello console!');
+// testing draggable.js
+import {Sortable} from '@shopify/draggable';
+// console.log(new Sortable());
+
+new Sortable(document.querySelectorAll('ul'), {
+  draggable: 'li'
+});
+
+let keyHistory = [];
+document.addEventListener('keyup', event => {
+  console.log('key event', event.key);
+  /*keyHistory.push(event.key);
+  if (event.key === 'Enter') {
+    console.log(keyHistory);
+    keyHistory = [];
+  }*/
+});
+
+document.addEventListener('keydown', event => {
+  console.log('keydown:', event.key, event.keyCode);
+  if (event.keyCode === 13) {
+    console.log('You hit enter!');
+  }
+});
