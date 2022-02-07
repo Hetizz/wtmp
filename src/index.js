8943f2e04b66d19716949d72dadc84177af5f1a5
Hetizz <heta.huostila@metropolia.fi>
2022-02-07 14:18:45 +0200

    Week 3, task 2

--- src/index.js
+++ src/index.js
@@ -2,32 +2,22 @@ import SodexoData from './modules/sodexo-data.js';
 import FazerData from './modules/fazer-data';
 
 let language = 'fi';
+let menuOrder = 'asc';
 let currentMenu = SodexoData.coursesFi;
 let currentFazer = FazerData.coursesFi;
 
-const sodexoMenu = document.querySelector('#sodexoMenu');
-const fazerMenu = document.querySelector('#fazerMenu');
-let menuOrder = 'asc';
-
 /**
- *
- * Prints the menu on page
+ * Prints sodexo menu on page
+ * @param {Array} menu
+ * @param {string} targetId
  */
-const printMenu = () => {
-  sodexoMenu.innerHTML = '';
-  for (const item of currentMenu) {
+const printMenu = (menu, targetId) => {
+  const ulElement = document.querySelector('#' + targetId);
+  ulElement.innerHTML = '';
+  for (const item of menu) {
     const li = document.createElement('li');
     li.textContent = item;
-    sodexoMenu.appendChild(li);
-  }
-};
-
-const printFazer = () => {
-  fazerMenu.innerHTML = '';
-  for (const item of currentFazer) {
-    const li = document.createElement('li');
-    li.textContent = item;
-    fazerMenu.appendChild(li);
+    ulElement.appendChild(li);
   }
 };
 
@@ -39,52 +29,61 @@ const switchLanguage = () => {
     language = 'en';
     currentMenu = SodexoData.coursesEn;
     currentFazer = FazerData.coursesEn;
+    printMenu(SodexoData.coursesEn, 'sodexoMenu');
+    printMenu(FazerData.coursesEn, 'fazerMenu');
   } else {
     language = 'fi';
     currentMenu = SodexoData.coursesFi;
-    currentFazer = SodexoData.coursesFi;
+    currentFazer = FazerData.coursesFi;
+    printMenu(SodexoData.coursesFi, 'sodexoMenu');
+    printMenu(FazerData.coursesFi, 'fazerMenu');
   }
 };
 
 /**
  *
- * @param {Array} courses
+ * @param {Array} courses1
+ * @param {Array} courses2
  * @param {string} order
  * @returns {Array}
+ * @returns {Array}
  */
-const sortCourses = (courses, order) => {
-  let sortedCourses = courses.sort();
+const sortCourses = (courses1, courses2, order) => {
+  let sortedSodexo = courses1.sort();
+  let sortedFazer = courses2.sort();
   if (order === 'asc') {
     menuOrder = 'desc';
   } else {
     menuOrder = 'asc';
-    sortedCourses = courses.reverse();
+    sortedSodexo = courses1.reverse();
+    sortedFazer = courses2.reverse();
   }
-  return sortedCourses;
+
+  printMenu(sortedSodexo, 'sodexoMenu');
+  printMenu(sortedFazer, 'fazerMenu');
+
+  return sortedSodexo, sortedFazer;
 };
 
 /**
  * picks a random dish from menu
- * @param {array} courses
+ *
  */
-const pickRandom = (courses) => {
+const pickRandom = () => {
   const randomIndex = Math.floor(Math.random() * currentMenu.length);
   const randomIndexF = Math.floor(Math.random() * currentFazer.length);
   alert('Sodexo: ' + currentMenu[randomIndex] + ' \nFazer: ' + currentFazer[randomIndexF]);
 };
 
-printMenu();
-printFazer();
+printMenu(SodexoData.coursesFi, 'sodexoMenu');
+printMenu(FazerData.coursesFi, 'fazerMenu');
 
 document.querySelector('#vaihto').addEventListener('click', () => {
   switchLanguage();
-  printMenu();
-  printFazer();
 });
 
 document.querySelector('#sort').addEventListener('click', () => {
-  sortCourses(currentMenu, menuOrder);
-  printMenu();
+  sortCourses(currentMenu, currentFazer, menuOrder);
 });
 
 document.querySelector('#random').addEventListener('click', () => {
